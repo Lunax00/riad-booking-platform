cf04fe326aac2c68d4e21c77d2f60de9e4f2401b
Lunax00 <142691923+Lunax00@users.noreply.github.com>
2025-12-27 14:41:57 +0100

    main-project-backend
    spring microservices

---
+++ riad-booking-platform_backend/src/main/java/com/lunaire/riadbookingplatform_backend/RiadBookingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lunaire.riadbookingplatform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiadBookingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiadBookingPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ riad-booking-platform_backend/src/test/java/com/lunaire/riadbookingplatform_backend/RiadBookingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunaire.riadbookingplatform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiadBookingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
